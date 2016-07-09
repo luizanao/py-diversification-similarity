b392f954dc90a75b95fc0f2bc14346dd4e8aa673
Ivo Lima <ivo.romario@gmail.com>
2016-07-08 21:06:04 -0300

    creates a visual dictionary using kmeans

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import cv2
+import numpy as np
 
 extractors = [("sift", cv2.SIFT()), ("surf", cv2.SURF())]
 SUPPORTED_DESCRIPTORS = dict(extractors)
@@ -10,4 +11,14 @@ def extract_descriptors(image, descriptor='sift'):
         raise Exception("{0} descriptor not implemented".format(descriptor))
     keypoints, descriptors = SUPPORTED_DESCRIPTORS[descriptor].detectAndCompute(image, None)
     return descriptors
+
+
+def create_codebook(samples, size=300):
+    """ Given a list of samples and the number of clusters, it applies a clusterization over all
+    descriptors and generates a visual dictionary named codebook"""
+    attempts = 1
+    data = np.array(samples)
+    criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_MAX_ITER, 100, 0.1)
+    ret, labels, centroids = cv2.kmeans(data, size, criteria, attempts, cv2.KMEANS_PP_CENTERS)
+    return centroids
 
